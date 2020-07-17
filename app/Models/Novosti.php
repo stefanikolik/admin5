66ee6cf24880bd9415cccc265cd8b273c6487e96
Stefan M. Nikolik <Stefan_Nikolik@moi.gov.mk>
2020-07-17 15:06:11 +0200

    admin5

---
+++ app/Models/Novosti.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Novosti extends Model{
+    protected $table = 'novosti';
+    public $timestamps = false;
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Admin - KumanovoNews')
+
+@section('content')
+<div class="container">
+    <div class="row">
+       <h1>drn drn </h1>
+
+    </div>
+</div>
+@endsection
